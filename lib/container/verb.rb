1a43e1e796e3cc9d1a30f08f98e2490033296a30
Ed Howland <ed.howland@gmail.com>
2015-04-29 20:32:19 -0400

    finished verb.rb

--- lib/container/verb.rb
+++ lib/container/verb.rb
@@ -1,10 +1,18 @@
 # verb.rb - Verb
 
 class Verb
-  def initialize(name, arg, glags=[])
+  def initialize(name, arg, *flags)
     @name=name
     @arg=arg
     @flags=flags
+
+    @value = [@name]
+  @value << @flags.join(' ') if !@flags.empty?
+    @value << @arg
     end
+
+  def to_s
+    @value.join(' ')
+  end
 end
 
--- lib/specs/verb_spec.rb
+++ lib/specs/verb_spec.rb
@@ -4,7 +4,23 @@ require 'minitest/autorun'
 require '../container/verb'
 
 describe Verb do
+  describe 'with name, flags and arg' do
   before do
-    @verb=Verb.new 'create', 'date', '-v', '--name=name', '-v jjj'
+    @verb=Verb.new 'create', 'date', '-c', '--name=name', '-v jjj'
+  end
+
+  it 'should be: create -c --name=name -v jjj arg' do
+    @verb.to_s.must_equal 'create -c --name=name -v jjj date'
+  end
+  end
+
+  describe 'with name and arg only' do
+    before do
+      @verb=Verb.new('start', 'name')
+    end
+
+    it 'should be: start name' do
+        @verb.to_s.must_equal 'start name'
+      end
   end
 end
