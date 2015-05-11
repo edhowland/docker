6db634e9cb3e2c310b13ba935bf72fc0ed263a3b
edhowland <ed.howland@gmail.com>
2015-05-11 16:50:29 +0000

    finished Config

--- lib/specs/config_spec.rb
+++ lib/specs/config_spec.rb
@@ -2,12 +2,44 @@
 
 require 'minitest/autorun'
 require '../tasks/config'
+
+describe Config do
+  before do
+    @c = Config.new
+  @c.image_name = 'pdftk'
+    @c.container_name = 'piper.pdftk'
+  @c.registry = 'swinehart'
+  @c.tag = 'v0.1'
+  end
+describe 'saving Config object' do
+  it 'should write out config' do
+    @c.save('spec.yml')
+  end
+end
+
+
 describe 'loading YAML file' do
   before do
-
+    @c = Config.load('spec.yml')
   end
 
-  it 'should be image_name : swinehart/juju:v0.1' do
-
+  it 'should be instance of Config' do
+    @c.must_be_instance_of Config
   end
+
+  it 'should be image_name :pdftk ' do
+    @c.image_name.must_equal 'pdftk'
+  end
+  it 'should registry be : swinehart' do
+    @c.registry.must_equal 'swinehart'
+  end
+
+  it 'should container_name be : piper.pdftk' do
+    @c.container_name.must_equal 'piper.pdftk'
+  end
+
+  it 'should tagg be : v0.1' do
+    @c.tag.must_equal 'v0.1'
+  end
+end
 end
