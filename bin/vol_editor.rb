3b3ac7070fea247dc8390b1c9442ac617541dcf5
edhowland <ed.howland@gmail.com>
2015-05-14 20:50:30 +0000

    added hash of volumes to container_factory.rb

--- bin/vol_editor.rb
+++ bin/vol_editor.rb
@@ -1,15 +1,37 @@
 # vol_editor.rb - VolEditor
 
 class VolEditorWorker
+  alias_method :a, :add
+  alias_method :d, :delete
+
   def initialize stdin, stdout, hash
     @in = stdin
     @out = stdout
     @hash = hash
   end
 
+
+def add
+  end
+
+  def delete
+  end
+  def request char
+  end
+def response
+  ''
+  end 
   # Read, Eval, Print Loop
   def repl
   @out.puts '(a)dd, (d)elete or (q)uit : '
   command = @in.gets
   end
 end
+
+class VolEditor
+  def initialize hash={}
+    VolEditorWorker.new STDIN, STDOUT, hash
+  end
+end
+
+
