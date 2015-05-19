af898f198bd4b4e745c9807463b3c0f1f3b14bd1
edhowland <ed.howland@gmail.com>
2015-05-19 20:12:46 +0000

    added container_factory_spec.rb

--- lib/specs/container_factory_spec.rb
+++ lib/specs/container_factory_spec.rb
@@ -6,14 +6,20 @@ CONFIG_ROOT='.'
 describe ContainerFactory do
   before do
     config = Config.new
-  config.container_name = 'piper.pdftk'
+    config.container_name = 'piper.pdftk'
+    config.registry = 'swineheart'
     config.image_name = 'pdftk'
+    config.tag = 'v0.1'
     config.arg = 'run.sh'
-config.save  CONFIG_ROOT + '/pdftk.yml'
+    config.save  CONFIG_ROOT + '/pdftk.yml'
     @c = ContainerFactory.load CONFIG_ROOT, 'pdftk'
   end
 
-  it 'should be : sudo docker start piper.pdftk' do
-  @c.start.must_equal 'sudo docker start piper.pdftk'
+  it "should create be : sudo docker create --name='piper.pdftk' swineheart/pdftk:v0.1 run.sh" do
+    @c.create.must_equal "sudo docker create --name='piper.pdftk' swineheart/pdftk:v0.1 run.sh"
+  end
+
+  it 'should start be : sudo docker start piper.pdftk' do
+    @c.start.must_equal 'sudo docker start piper.pdftk'
   end
 end
