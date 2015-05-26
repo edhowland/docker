b36612108904a40121dade91ea160c7a363bd237
edhowland <ed.howland@gmail.com>
2015-05-26 15:11:04 +0000

    \'extracted method file_mocker. takes a block to set inner file expectations\'

--- templates/filer_spec.rb
+++ templates/filer_spec.rb
@@ -3,14 +3,28 @@
 require 'minitest/autorun'
 require './filer'
 
+def file_mocker(name, mode, &blk)
+  fmock = MiniTest::Mock.new
+  yield(fmock)
+  mock = MiniTest::Mock.new
+  mock.expect(:open, true) do |a1, a2, &block|
+    block.call(fmock)
+    a1 == name && a2 == mode && fmock.verify
+  end
+  mock
+end
 describe "Mocking" do
   before do
-        @mock = MiniTest::Mock.new
-    @mock.expect(:open,true) do |a1, a2, &blk|
-          fmock = MiniTest::Mock.new  
+        #@mock = MiniTest::Mock.new
+    #@mock.expect(:open,true) do |a1, a2, &blk|
+          #fmock = MiniTest::Mock.new  
+      #fmock.expect(:puts, nil, ['hello'])
+      #blk.call(fmock)
+      #a1 == 'file.txt' && a2 == 'w' && fmock.verify
+    #end
+
+    @mock = file_mocker('file.txt', 'w') do |fmock|
       fmock.expect(:puts, nil, ['hello'])
-      blk.call(fmock)
-      a1 == 'file.txt' && a2 == 'w' && fmock.verify
     end
   end
   it "should do something" do
