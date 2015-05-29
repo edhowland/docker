e4f0fe1969296510bfde01f0f351dcfe9cf9b8cc
edhowland <ed.howland@gmail.com>
2015-05-29 19:22:26 +0000

    \'finished container_spec.rb\'

--- lib/specs/container_spec.rb
+++ lib/specs/container_spec.rb
@@ -1,8 +1,9 @@
-# container_spec.rb - specs for lib/container/container.rb
-
 require 'minitest/autorun'
 require '../container'
 
+def build_container(hash={})
+  Container.new('image', 'name', 'command', hash)
+end
 describe Container do
   describe 'with full options' do
     before do
@@ -11,74 +12,47 @@ describe Container do
     it 'should have a create method' do
       @c.must_respond_to :create
     end
-
-    it 'should not be empty' do
-      @c.create.wont_be_empty
-    end
-
-    it 'should be : create --name=\'name\' image command' do
+    it 'should be create ...' do
       @c.create.must_equal "create --name='name' image command"
     end
 
     it 'should have a start method' do
       @c.must_respond_to :start
     end
-
-    it 'should not be empty' do
-      @c.start.wont_be_empty
-    end
-
     it 'should be "start name"' do
       @c.start.must_equal 'start name'
     end
   end
-
-  describe 'with volumes' do
+  describe 'with volume hash' do
     before do
-      @c = Container.new('image', 'name', 'command',
-                         '/d1/d2' => '/v1/v2', '/d3/d4' => '/v3/v4')
+      @c = build_container '/d1/d2' => '/v1/v2', '/d3/d4' => '/v3/v4'
     end
-    it 'should have vols_hash method' do
-      @c.must_respond_to :vols_hash
-    end
-    it 'should not have empty vols_hash' do
-      @c.vols_hash.wont_be_nil
-      @c.vols_hash.wont_be_empty
-    end
-
     # rubocop:disable Metrics/LineLength
-    it 'should match host dirs to instance dirs' do
+    it 'should have volumes' do
       @c.create.must_equal("create --name='name' -v /d1/d2:/v1/v2 -v /d3/d4:/v3/v4 image command")
-      end
-    # rubocop:enable Metrics/LineLength
     end
-
-    describe 'with verbs : rm and wait' do
-      before do
-        @c = Container.new 'image', 'name', 'command'
-      end
-
-      it 'should have verb : rm' do
-        @c.must_respond_to :rm
-      end
-
-    it 'should rm be : rm name' do
+    # rubocop:enable Metrics/LineLength
+  end
+  describe 'with verbs rm, wait' do
+    before do
+      @c = build_container
+    end
+    it 'should have verb rm' do
+      @c.must_respond_to :rm
+    end
+    it 'should rm be: rm name' do
       @c.rm.must_equal 'rm name'
     end
-
-    it 'should have verb  : wait' do
+    it 'should have verb wait' do
       @c.must_respond_to :wait
     end
-
-    it 'should wait be :wait' do
+    it 'should wait be: wait' do
       @c.wait.must_equal 'wait'
     end
-
-    it 'should have verb : start_then_wait' do
+    it 'should have start_then_wait' do
       @c.must_respond_to :start_then_wait
     end
-
-    it 'should start_then_wait be : sha=$(start name); wait ${sha}' do
+    it 'should start_then_wait be: ...' do
       @c.start_then_wait.must_equal 'sha=$(start name); wait ${sha}'
     end
   end
