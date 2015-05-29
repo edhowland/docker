57979998c9af15badd56fe80f367f79e36f13fc0
edhowland <ed.howland@gmail.com>
2015-05-29 16:11:50 +0000

    \'disable cop line length for lib/container/container_factory.rb\'

--- lib/container/container_factory.rb
+++ lib/container/container_factory.rb
@@ -3,10 +3,13 @@
 require "#{File.dirname(__FILE__)}/../tasks/config"
 require "#{File.dirname(__FILE__)}/../command"
 require "#{File.dirname(__FILE__)}/container"
+# rubocop:disable Metrics/LineLength
 
+# TODO: Describe ContainerFactory
 class ContainerFactory
   def self.load(root, name)
     config = Config.load("#{root}/#{name}.yml")
-    Sudo.new + Docker.new + Container.new(config.image_full_name, config.container_name, config.arg, config.vols_hash)
+    Sudo.new + Docker.new +
+      Container.new(config.image_full_name, config.container_name, config.arg, config.vols_hash)
   end
 end
