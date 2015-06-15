23dddca6652e148cfd9d281b0fb28adefce6bbae
edhowland <ed.howland@gmail.com>
2015-06-15 17:19:14 +0000

    \'added rake methods to spec_rake.rb\'

--- spec/integration/spec_rake.rb
+++ spec/integration/spec_rake.rb
@@ -1,9 +1,19 @@
-# spec_rake.rb - class SpecRake
+# specrake.rb - Class for SpecRake
 
+# TODO: describe SpecRake
 class SpecRake
   def initialize dir
+    @dir = dir
+    FileUtils.cd(@dir) do
+      @rake = Rake.application
+      @rake.init
+      @rake.load_rakefile
+    end
   end
 
   def method_missing sym
+    FileUtils.cd(@dir) do
+      @rake.invoke sym.to_s
+    end
   end
 end
