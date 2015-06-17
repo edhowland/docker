f93ef7f7808b28e6cfa73f5e538f37117fff0a1a
edhowland <ed.howland@gmail.com>
2015-06-17 00:07:53 +0000

    \'added pipeline_run_spec.rb\'

---
+++ spec/integration/pipeline_run_spec.rb
@@ -0,0 +1,12 @@
+# pipeline_run_spec.rb - spec to execute pipeline/Rakefile and check it
+
+
+require 'minitest/autorun'
+
+describe 'pipeline' do
+  before do
+    # setup rake for ../../pipeline/Rakefile
+    # @rake.clean
+    #@rake.pdftk
+  end
+end
