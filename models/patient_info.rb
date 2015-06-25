2fb59fdd1ff5c8c72209a6b7b1723535ffc6c97a
edhowland <ed.howland@gmail.com>
2015-06-25 13:18:04 +0000

    \'added fields to models/patient_info.rb\'

---
+++ models/patient_info.rb
@@ -0,0 +1,26 @@
+# patient_info.rb - model class PatientInfo
+
+class PatientInfo
+  def initialize arr
+    @name = arr[0]
+  @dob = arr[1]
+    @mrn = arr[2]
+    @gender = arr[3]
+    @referring_physician = arr[4]
+    @referring_pathologist = arr[5]
+    @accession_number = arr[6]
+    @specimen_type = arr[7]
+    @tissue_type = arr[8]
+    @indication = arr[9]
+    @date_collected = arr[10]
+    @date_ordered = arr[11]
+
+  end
+
+  attr_reader :name
+  attr_reader :dob
+  attr_reader :mrn
+    attr_reader :gender
+    attr_reader :referring_physician
+    attr_reader :referring_pathologist
+end
