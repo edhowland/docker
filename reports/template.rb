fee69bfd379637694dd47cf5c6e9c571752713b6
edhowland <ed.howland@gmail.com>
2015-06-25 18:57:13 +0000

    \'added looping construct to template.rb\'

--- reports/template.rb
+++ reports/template.rb
@@ -11,9 +11,15 @@ orm = Orm.new './patient_info.sqlite3', PatientInfo
 field_list = %w[ patient_name  date_of_birth mrn gender referring_physician referring_pathologist accession_number specimen_type tissue_type indication  date_collected date_ordered ].join(',')
 
 rows = orm.select field_list
+
 patient_info = rows[0]
+rows.each do |p|
+  patient_info = p
+  File.open("#{patient_info.mrn}.html", 'w') do |f|
 
 simple_template = File.read(ARGV.first)
 
 renderer = ERB.new(simple_template)
-puts output = renderer.result()
+    f.puts renderer.result()
+  end
+end
