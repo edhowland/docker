0d81608c30f15c0f41406ac622daf46bc258f929
edhowland <ed.howland@gmail.com>
2015-06-25 13:55:34 +0000

    \'basic spec for Orm working\'

--- lib/orm/orm.rb
+++ lib/orm/orm.rb
@@ -1,6 +1,12 @@
 # orm.rb - class Orm
 
 class Orm
-  def initialize dbpath
+  def initialize dbpath, model
+    @dbpath = dbpath
+    @model = model
   end
+
+  def select fields_str
+    [@model.new([])]
+  end 
 end
--- spec/orm_spec.rb
+++ spec/orm_spec.rb
@@ -1,12 +1,16 @@
 # orm_spec.rb - spec for class Orm
 
+require '../models/patient_info'
 require '../lib/orm'
 require 'minitest/autorun'
 
 
 describe Orm do
-  let(:orm) { Orm.new('path') }
+  let(:orm) { Orm.new('path', PatientInfo) }
   describe 'select' do
+    subject { orm.select('patient_name') }
+
+  specify { subject[0].must_be_instance_of PatientInfo  }
 
   end
 
