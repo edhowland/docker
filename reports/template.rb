3ae3e273ca65246792b160495772eaf8e6d2b607
edhowland <ed.howland@gmail.com>
2015-06-29 15:17:25 +0000

    \'added -p, -t opts to template.rb\'

--- reports/template.rb
+++ reports/template.rb
@@ -7,8 +7,16 @@ require '../lib/support'
 require'../models/patient_info'
 require '../lib/orm'
 
+template = 'template.html' # default template unless -t option is given
+filter = {}
 
 parser = OptionParser.new do |opts|
+  opts.on('-t', '--template', 'The template.html touse') do 
+    template = 'template.html'
+  end
+  opts.on('-p', '--patient', 'Selects the patient with this name') do
+    filter = {where: "patient_name = \"FooBar\"" }
+  end
   opts.on('-h', '--help', 'Displays this help') do
     puts opts
     exit
@@ -22,7 +30,7 @@ parser.parse!
 orm = Orm.new './patient_info.sqlite3', PatientInfo
 field_list = %w[ patient_name  date_of_birth mrn gender referring_physician referring_pathologist accession_number specimen_type tissue_type indication  date_collected date_ordered ].join(',')
 
-rows = orm.select field_list
+rows = orm.select field_list, filter
 
 patient_info = rows[0]
 rows.each do |p|
