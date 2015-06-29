3cc5f4b3a8a2c73e81488ac699f30af6b5010818
edhowland <ed.howland@gmail.com>
2015-06-29 16:20:23 +0000

    \'fixed --template name of opts for template.rb\'

--- reports/template.rb
+++ reports/template.rb
@@ -15,7 +15,7 @@ parser = OptionParser.new do |opts|
       opts.separator ""
       opts.separator "Specific options:"
 
-  opts.on('-t', '--template', 'The template.html touse') do  |file|
+  opts.on('--template file', String, 'The template.html touse') do  |file|
     template = file
   end
   opts.on('-p', '--patient', 'Selects the patient with this name') do |name|
