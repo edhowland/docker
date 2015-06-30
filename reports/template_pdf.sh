d8d6af482e008a31db6837e177b90525b837250b
edhowland <ed.howland@gmail.com>
2015-06-30 20:13:52 +0000

    \'added steps to install wkhtmltopdf in how.to.install.wkhtmltopdf.from.deb\'

--- reports/template_pdf.sh
+++ reports/template_pdf.sh
@@ -1,7 +1,6 @@
 # template_pdf.sh - call wkhtmltopdf.sh with args for template.html ...
 
-
 html=$1
 pdf=$2
 
-./wkhtmltopdf.sh --user-style-sheet page.css --custom-header header.html $html $pdf
+./wkpdf.sh --user-style-sheet page.css --custom-header header.html $html $pdf
