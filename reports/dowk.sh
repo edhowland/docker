4efe43de67582eee21c9283fc1acffeaa51076d4
edhowland <ed.howland@gmail.com>
2015-07-01 16:13:55 +0000

    \'added line break between header and content\'

--- reports/dowk.sh
+++ reports/dowk.sh
@@ -1,4 +1,4 @@
-wkhtmltopdf --user-style-sheet page.css  --header-html file:///home/vagrant/src/docker/reports/header.html MRN_123456.html MRN_123456.pdf
+wkhtmltopdf --user-style-sheet page.css --header-html file:///home/vagrant/src/docker/reports/header.html --header-line MRN_123456.html MRN_123456.pdf
 # --print-media-type  --header-spacing 3 
 
 
