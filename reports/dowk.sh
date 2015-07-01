4fdde065b93a526cd4d97a76d75cba8121466d57
edhowland <ed.howland@gmail.com>
2015-07-01 16:22:40 +0000

    \'improved template.html to have 3 line breaks\'

--- reports/dowk.sh
+++ reports/dowk.sh
@@ -1,4 +1,5 @@
-wkhtmltopdf --user-style-sheet page.css --header-html file:///home/vagrant/src/docker/reports/header.html --header-line MRN_123456.html MRN_123456.pdf
+# dowk.sh - calls wkhtmltopdf with right options
+wkhtmltopdf --user-style-sheet page.css --header-html file:///home/vagrant/src/docker/reports/header.html --header-line $@
 # --print-media-type  --header-spacing 3 
 
 
--- reports/template_pdf.sh
+++
@@ -1,6 +0,0 @@
-# template_pdf.sh - call wkhtmltopdf.sh with args for template.html ...
-
-html=$1
-pdf=$2
-
-./wkpdf.sh --user-style-sheet page.css --custom-header header.html $html $pdf
